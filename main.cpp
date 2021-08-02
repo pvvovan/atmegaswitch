2e304939c9031ff346eed1c4731de2b02ba0d45d
PVVovan <pvvovan@gmail.com>
2021-08-02 22:08:53 +0200

    Add portSAVE_CONTEXT()

--- main.cpp
+++ main.cpp
@@ -236,6 +236,51 @@ BaseType_t xTaskCreate2( TaskFunction_t pxTaskCode,
                                 "pop    __tmp_reg__                             \n\t"   \
                              );
 
+#define portSAVE_CONTEXT()                                                              \
+        __asm__ __volatile__ (  "push   __tmp_reg__                             \n\t"   \
+                                "in     __tmp_reg__, __SREG__                   \n\t"   \
+                                "cli                                            \n\t"   \
+                                "push   __tmp_reg__                             \n\t"   \
+                                "push   __zero_reg__                            \n\t"   \
+                                "clr    __zero_reg__                            \n\t"   \
+                                "push   r2                                      \n\t"   \
+                                "push   r3                                      \n\t"   \
+                                "push   r4                                      \n\t"   \
+                                "push   r5                                      \n\t"   \
+                                "push   r6                                      \n\t"   \
+                                "push   r7                                      \n\t"   \
+                                "push   r8                                      \n\t"   \
+                                "push   r9                                      \n\t"   \
+                                "push   r10                                     \n\t"   \
+                                "push   r11                                     \n\t"   \
+                                "push   r12                                     \n\t"   \
+                                "push   r13                                     \n\t"   \
+                                "push   r14                                     \n\t"   \
+                                "push   r15                                     \n\t"   \
+                                "push   r16                                     \n\t"   \
+                                "push   r17                                     \n\t"   \
+                                "push   r18                                     \n\t"   \
+                                "push   r19                                     \n\t"   \
+                                "push   r20                                     \n\t"   \
+                                "push   r21                                     \n\t"   \
+                                "push   r22                                     \n\t"   \
+                                "push   r23                                     \n\t"   \
+                                "push   r24                                     \n\t"   \
+                                "push   r25                                     \n\t"   \
+                                "push   r26                                     \n\t"   \
+                                "push   r27                                     \n\t"   \
+                                "push   r28                                     \n\t"   \
+                                "push   r29                                     \n\t"   \
+                                "push   r30                                     \n\t"   \
+                                "push   r31                                     \n\t"   \
+                                "lds    r26, pxCurrentTCB                       \n\t"   \
+                                "lds    r27, pxCurrentTCB + 1                   \n\t"   \
+                                "in     __tmp_reg__, __SP_L__                   \n\t"   \
+                                "st     x+, __tmp_reg__                         \n\t"   \
+                                "in     __tmp_reg__, __SP_H__                   \n\t"   \
+                                "st     x+, __tmp_reg__                         \n\t"   \
+                             );
+
 static void fast([[maybe_unused]] void* p);
 
 static void slow([[maybe_unused]] void* p)
@@ -246,6 +291,8 @@ static void slow([[maybe_unused]] void* p)
   for (int i = 0; i < 6; i++) {
     _delay_ms(1000);
     PORTB ^= PB5_MASK;
+    portSAVE_CONTEXT();
+    portRESTORE_CONTEXT();
   }
 
   xTaskCreate2(
@@ -264,6 +311,8 @@ static void fast([[maybe_unused]] void* p)
   for (int i = 0; i < 50; i++) {
     _delay_ms(100);
     PORTB ^= PB5_MASK;
+    portSAVE_CONTEXT();
+    portRESTORE_CONTEXT();
   }
 
   xTaskCreate2(
